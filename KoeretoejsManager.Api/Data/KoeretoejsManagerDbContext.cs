8536ad1e589d75b5ebf62bc468c731fb4be30f25
sko_bjki <sko_bjki@itcn.dk>
2026-04-21 10:12:06 +0200

    Added data folder to .Api. Created DbContext and setup the sqlite connection in program in .Api.

---
+++ KoeretoejsManager.Api/Data/KoeretoejsManagerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KoeretoejsManager.Api.Data
+{
+    public class KoeretoejsManagerDbContext : DbContext
+    {
+        public KoeretoejsManagerDbContext(DbContextOptions<KoeretoejsManagerDbContext> options) : base(options)
+        {
+            
+        }
+
+        //public DbSet<Todo> Todos { get; set; }
+    }
+}
--- KoeretoejsManager.Api/Program.cs
+++ KoeretoejsManager.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using KoeretoejsManager.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace KoeretoejsManager.Api
 {
     public class Program
@@ -8,6 +11,9 @@ namespace KoeretoejsManager.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<KoeretoejsManagerDbContext>(options =>
+                options.UseSqlite("Data Source=koeretoejsmanager.db"));
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
