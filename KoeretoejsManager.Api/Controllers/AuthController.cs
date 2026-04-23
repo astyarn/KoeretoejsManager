fc2ac8bf08b9a5c76b0c6154d26ac29ca552b6c0
sko_bjki <sko_bjki@itcn.dk>
2026-04-23 09:54:15 +0200

    Added an AuthController. Added UserloginRequest helper model

---
+++ KoeretoejsManager.Api/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using KoeretoejsManager.Api.Interfaces;
+using KoeretoejsManager.Api.Models.HelperModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoeretoejsManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserAuthService _userService;
+        private readonly IJwtService _jwtService;
+
+        public AuthController(IUserAuthService userService, IJwtService jwtService)
+        {
+            _userService = userService;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] UserLoginRequest request)
+        {
+            var user = _userService.ValidateUser(request.Username, request.Password);
+
+            if (user == null)
+                return Unauthorized("Invalid credentials");
+
+            var token = _jwtService.GenerateToken(user);
+
+            return Ok(new
+            {
+                accessToken = token,
+                expiresIn = 900
+            });
+        }
+    }
+}
---
+++ KoeretoejsManager.Api/Models/HelperModels/UserLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace KoeretoejsManager.Api.Models.HelperModels
+{
+    public class UserLoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
