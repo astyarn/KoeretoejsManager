e7667bcc029f933affa9e61dab9944c6f6ee695c
sko_bjki <sko_bjki@itcn.dk>
2026-04-23 09:25:33 +0200

    Created UserAuthService to handle user authentications logic

---
+++ KoeretoejsManager.Api/Interfaces/IUserAuthService.cs
@@ -0,0 +1,9 @@
+﻿using KoeretoejsManager.Api.Models;
+
+namespace KoeretoejsManager.Api.Interfaces
+{
+    public interface IUserAuthService
+    {
+        User ValidateUser(string username, string password);
+    }
+}
---
+++ KoeretoejsManager.Api/Services/UserAuthService.cs
@@ -0,0 +1,32 @@
+﻿using KoeretoejsManager.Api.Data;
+using KoeretoejsManager.Api.Interfaces;
+using KoeretoejsManager.Api.Models;
+
+namespace KoeretoejsManager.Api.Services
+{
+    public class UserAuthService : IUserAuthService
+    {
+        private readonly KoeretoejsManagerDbContext _db;
+
+        public UserAuthService(KoeretoejsManagerDbContext db)
+        {
+            _db = db;
+        }
+
+        public User ValidateUser(string username, string password)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.UserName == username);
+
+            if (user == null)
+                return null;
+
+            //TODO: Implement password hashing and verification
+            //if (!BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
+            //    return null;
+            if ((password != user.PasswordHash))
+                return null;
+
+            return user;
+        }
+    }
+}
