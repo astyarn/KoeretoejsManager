6a670936d6650776b2827e6284751989d5f50ef9
sko_bjki <sko_bjki@itcn.dk>
2026-04-24 08:28:14 +0200

    Not finished implementation of CustomAuthStateProvider.

---
+++ KoeretoejsManager/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using KoeretoejsManager.Interfaces;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace KoeretoejsManager.Api.Services
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly IUserTokenStore _userTokenStore;
+
+        public CustomAuthStateProvider(IUserTokenStore userTokenStore)
+        {
+            _userTokenStore = userTokenStore;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = _userTokenStore.GetToken();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return Task.FromResult(
+                    new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()))
+                );
+            }
+
+            //TODO Get Blazor to remember the correct user after refresh, by parsing the token and creating a ClaimsPrincipal with the correct claims.
+
+            return Task.FromException<AuthenticationState>(new NotImplementedException());
+        }
+
+        public void NotifyAuthStateChanged()
+        {
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+    }
+}
