7d816f46783801dd986f75c43034c23ef20e0fcb
sko_bjki <sko_bjki@itcn.dk>
2026-04-24 09:33:04 +0200

    Added Vehicle class and VehicleStatusType Enum.

---
+++ KoeretoejsManager.Api/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using KoeretoejsManager.Shared.Enums;
+
+namespace KoeretoejsManager.Api.Models
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string LicensePlate { get; set; }
+        public DrivingLicenseType RequiredLicense { get; set; }
+        public VehicleStatusType Status { get; set; }
+        public int NumberOfSeats { get; set; }
+
+        //property for location
+        //property for cargocapacity
+    }
+}
---
+++ KoeretoejsManager.Shared/Enums/VehicleStatusType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoeretoejsManager.Shared.Enums
+{
+    public enum VehicleStatusType
+    {
+        Available = 0,
+        InUse = 1,
+        Maintenance = 2,
+        Decommissioned = 3
+    }
+}
