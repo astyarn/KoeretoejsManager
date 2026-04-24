9ebca2b9c6d8762dc714b419e5bd67ecb619022b
sko_bjki <sko_bjki@itcn.dk>
2026-04-24 11:08:00 +0200

    Created VehicleService and its interface.

---
+++ KoeretoejsManager.Api/Interfaces/IVehicleService.cs
@@ -0,0 +1,12 @@
+﻿using KoeretoejsManager.Api.Models;
+using KoeretoejsManager.Shared.Enums;
+
+namespace KoeretoejsManager.Api.Interfaces
+{
+    public interface IVehicleService
+    {
+        List<Vehicle> GetAllVehicles();
+        List<Vehicle> GetAllVehiclesByDriversLicense(List<DrivingLicenseType> drivingLicenseTypes);
+        bool CreateVehicle(Vehicle vehicle);
+    }
+}
---
+++ KoeretoejsManager.Api/Services/VehicleService.cs
@@ -0,0 +1,35 @@
+﻿using KoeretoejsManager.Api.Data;
+using KoeretoejsManager.Api.Interfaces;
+using KoeretoejsManager.Api.Models;
+using KoeretoejsManager.Shared.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoeretoejsManager.Api.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        private readonly KoeretoejsManagerDbContext _db;
+
+        public VehicleService(KoeretoejsManagerDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateVehicle(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Vehicle> GetAllVehicles()
+        {
+            return _db.Vehicles.ToList();
+        }
+
+        public List<Vehicle> GetAllVehiclesByDriversLicense(List<DrivingLicenseType> drivingLicenseTypes)
+        {
+            return _db.Vehicles
+                .Where(v => drivingLicenseTypes.Contains(v.RequiredLicense))
+                .ToList();
+        }
+    }
+}
