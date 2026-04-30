22a192910c519322af0abcda4bc7fae5ff349892
sko_bjki <sko_bjki@itcn.dk>
2026-04-30 13:23:52 +0200

    Added UserPage. Added UserApiService. Created part of demo method to pull all available users for selection.

---
+++ KoeretoejsManager/Interfaces/IUserApiService.cs
@@ -0,0 +1,9 @@
+﻿using KoeretoejsManager.Shared.DTOs;
+
+namespace KoeretoejsManager.Interfaces
+{
+    public interface IUserApiService
+    {
+        Task<List<UserIdDTO>> GetAllUserIds();    //Demo
+    }
+}
---
+++ KoeretoejsManager/Services/UserApiService.cs
@@ -0,0 +1,26 @@
+﻿using KoeretoejsManager.Interfaces;
+using KoeretoejsManager.Shared.DTOs;
+
+namespace KoeretoejsManager.Services
+{
+    public class UserApiService : IUserApiService
+    {
+        private readonly HttpClient _http;
+        private readonly string _apiKey;
+
+        public UserApiService(HttpClient http, IConfiguration configuration)
+        {
+            _http = http;
+            _apiKey = configuration["ApiKey"] ?? throw new InvalidOperationException("ApiKey missing");
+
+            //Set default header for all requests using this HttpClient
+            _http.DefaultRequestHeaders.Remove("x-api-key");
+            _http.DefaultRequestHeaders.Add("x-api-key", _apiKey);
+        }
+
+        public async Task<List<UserIdDTO>> GetAllUserIds()
+        {
+            return await _http.GetFromJsonAsync<List<UserIdDTO>>("api/user/all-user-ids");
+        }
+    }
+}
