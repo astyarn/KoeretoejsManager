d89b71b2642d1178c27d62332f585ff870bdac2a
sko_bjki <sko_bjki@itcn.dk>
2026-04-30 08:42:23 +0200

    Added setting to make enum convertable to string with serilisation.

--- KoeretoejsManager.Shared/Enums/DrivingLicenseType.cs
+++ KoeretoejsManager.Shared/Enums/DrivingLicenseType.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace KoeretoejsManager.Shared.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum DrivingLicenseType
     {
         Unknown = 0,    //In case of missing or invalid data
--- KoeretoejsManager.Shared/Enums/VehicleStatusType.cs
+++ KoeretoejsManager.Shared/Enums/VehicleStatusType.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace KoeretoejsManager.Shared.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum VehicleStatusType
     {
         Available = 0,
