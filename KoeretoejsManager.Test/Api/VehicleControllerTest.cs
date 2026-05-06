af5a2b9b6a765964eb25233d4de179e0fbc61ba5
sko_bjki <sko_bjki@itcn.dk>
2026-05-06 13:58:28 +0200

    Added proof-of-concept unit test. Two test to just see if the the controller response is correct, no data check. And then 3 test if the Vehicle service reponse correctly with data from an in memory database.

---
+++ KoeretoejsManager.Test/Api/VehicleControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using KoeretoejsManager.Api.Controllers;
+using KoeretoejsManager.Api.Interfaces;
+using KoeretoejsManager.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoeretoejsManager.Test.Api
+{
+    public class VehicleControllerTest
+    {
+        private readonly Mock<IVehicleService> _vehicleServiceMock;
+        private readonly VehicleController _controller;
+
+        public VehicleControllerTest()
+        {
+            _vehicleServiceMock = new Mock<IVehicleService>();
+            _controller = new VehicleController(_vehicleServiceMock.Object);
+        }
+
+        [Fact]
+        public void GetAllVehicles_ReturnsOkResult_WithList()   //Just testing if this method returns an Ok Result with a list of vehicles, not testing the actual data
+        {
+            // Arrange
+            var vehicles = new List<VehicleDTO>
+            {
+                new VehicleDTO(),
+                new VehicleDTO()
+            };
+
+            _vehicleServiceMock.Setup(s => s.GetAllVehicles())
+                .Returns(vehicles);
+
+            // Act
+            var result = _controller.GetAllVehicles();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsType<List<VehicleDTO>>(okResult.Value);
+
+            Assert.Equal(2, returnValue.Count);
+        }
+
+        [Fact]
+        public void GetVehicleById_ReturnsNotFound_WhenVehicleDoesNotExist()    // Just testing if this method returns a NotFoundResult when the vehicle does not exist, not testing the actual data
+        {
+            // Arrange
+            _vehicleServiceMock.Setup(s => s.GetVehicleById(It.IsAny<int>()))
+                .Returns((VehicleDTO)null);
+
+            // Act
+            var result = _controller.GetVehicleById(1);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+    }
+}
---
+++ KoeretoejsManager.Test/Api/VehicleServiceTest.cs
@@ -0,0 +1,96 @@
+﻿using KoeretoejsManager.Api.Data;
+using KoeretoejsManager.Api.Models;
+using KoeretoejsManager.Api.Services;
+using KoeretoejsManager.Shared.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoeretoejsManager.Test.Api
+{
+    public class VehicleServiceTest
+    {
+        private KoeretoejsManagerDbContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<KoeretoejsManagerDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()) // isolated DB per test
+                .Options;
+
+            return new KoeretoejsManagerDbContext(options);
+        }
+
+        private VehicleService CreateService(KoeretoejsManagerDbContext db)
+        {
+            return new VehicleService(db);
+        }
+
+        [Fact]
+        public void GetAllVehicles_ReturnsAllVehicles()
+        {
+            // Arrange
+            var db = CreateDbContext();
+
+            db.Vehicles.AddRange(
+                new Vehicle { VehicleId = 1, LicensePlate = "AA111", RequiredLicense = DrivingLicenseType.B },
+                new Vehicle { VehicleId = 2, LicensePlate = "BB222", RequiredLicense = DrivingLicenseType.C }
+            );
+
+            db.SaveChanges();
+
+            var service = CreateService(db);
+
+            // Act
+            var result = service.GetAllVehicles();
+
+            // Assert
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public void GetVehicleById_ReturnsCorrectVehicle()
+        {
+            // Arrange
+            var db = CreateDbContext();
+
+            db.Vehicles.AddRange(
+                new Vehicle { VehicleId = 1, LicensePlate = "AA111", RequiredLicense = DrivingLicenseType.B },
+                new Vehicle { VehicleId = 2, LicensePlate = "BB222", RequiredLicense = DrivingLicenseType.C }
+            );
+
+            db.SaveChanges();
+
+            var service = CreateService(db);
+
+            // Act
+            var result = service.GetVehicleById(1);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("AA111", result.LicensePlate);
+        }
+
+        [Fact]
+        public void GetVehicleById_ReturnsCorrectVehicle2()
+        {
+            // Arrange
+            var db = CreateDbContext();
+
+            db.Vehicles.AddRange(
+                new Vehicle { VehicleId = 1, LicensePlate = "AA111", RequiredLicense = DrivingLicenseType.B },
+                new Vehicle { VehicleId = 2, LicensePlate = "BB222", RequiredLicense = DrivingLicenseType.C }
+            );
+
+            db.SaveChanges();
+
+            var service = CreateService(db);
+
+            // Act
+            var result = service.GetVehicleById(1);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEqual("BB222", result.LicensePlate);
+        }
+    }
+}
